dc5d1fe9834e4a058a5b4334bbb464c1b697d083
Marwa Hijazi <hijaziimarwa@gmail.com>
2025-08-05 02:28:26 +0300

    feat: create dynamic task elements with checkbox, text, edit and delete buttons and append to li

--- frontend-todo/script.js
+++ frontend-todo/script.js
@@ -5,4 +5,25 @@ const taskList = document.getElementById("task-list");
 function addTask() {
   if (taskInput.value === "") {
     alert("Please enter a task");
+  } else {
+    const taskText = taskInput.value;
+
+    const li = document.createElement("li");
+
+    const checkbox = document.createElement("span");
+    const textSpan = document.createElement("span");
+    textSpan.classList.add("task-text");
+    textSpan.textContent = taskText;
+
+    const editBtn = document.createElement("span");
+    const deleteBtn = document.createElement("span");
+
+    checkbox.classList.add("fa", "fa-circle-o", "checkbox");
+    editBtn.classList.add("fa", "fa-edit", "edit-btn");
+    deleteBtn.classList.add("fa", "fa-trash", "delete-btn");
+
+    li.append(checkbox, textSpan, editBtn, deleteBtn);
+    taskList.appendChild(li);
     
+  }
+}
