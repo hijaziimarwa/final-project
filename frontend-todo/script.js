709d1b2274ebecb7e4d9fb525b466013479f023f
Marwa Hijazi <hijaziimarwa@gmail.com>
2025-08-05 02:30:08 +0300

    feat: initialize DOM references for task input, add button, and task list container

--- frontend-todo/script.js
+++ frontend-todo/script.js
@@ -6,24 +6,6 @@ function addTask() {
   if (taskInput.value === "") {
     alert("Please enter a task");
   } else {
-    const taskText = taskInput.value;
-
-    const li = document.createElement("li");
-
-    const checkbox = document.createElement("span");
-    const textSpan = document.createElement("span");
-    textSpan.classList.add("task-text");
-    textSpan.textContent = taskText;
-
-    const editBtn = document.createElement("span");
-    const deleteBtn = document.createElement("span");
-
-    checkbox.classList.add("fa", "fa-circle-o", "checkbox");
-    editBtn.classList.add("fa", "fa-edit", "edit-btn");
-    deleteBtn.classList.add("fa", "fa-trash", "delete-btn");
-
-    li.append(checkbox, textSpan, editBtn, deleteBtn);
-    taskList.appendChild(li);
     
   }
 }
